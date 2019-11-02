16dfecd5c1c60f41dc2023e4cf357291835f5737
josh-lind <lind.98@osu.edu>
2019-11-02 13:49:18 -0400

    attack and get boost

--- src/bot.py
+++ src/bot.py
@@ -6,26 +6,28 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 from util.orientation import Orientation
 from util.vec import Vec3
 
+# from RLUtilities.GameInfo import GameInfo
+# from RLUtilities.Simulation import Input
+# from RLUtilities.LinearAlgebra import norm
 
 class MyBot(BaseAgent):
 
     def initialize_agent(self):
         # This runs once before the bot starts up
         self.controller_state = SimpleControllerState()
+        self.maneuver = None
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        ball_location = Vec3(packet.game_ball.physics.location)
+        self.set_maneuver(packet)
 
         my_car = packet.game_cars[self.index]
         car_location = Vec3(my_car.physics.location)
-
-        car_to_ball = ball_location - car_location
 
         # Find the direction of our car using the Orientation class
         car_orientation = Orientation(my_car.physics.rotation)
         car_direction = car_orientation.forward
 
-        steer_correction_radians = find_correction(car_direction, car_to_ball)
+        steer_correction_radians = find_correction(car_direction, self.get_target(packet) - car_location)
 
         if steer_correction_radians > 0:
             # Positive radians in the unit circle is a turn to the left.
@@ -69,3 +71,39 @@ def draw_debug(renderer, car, ball, action_display):
     # print the action that the bot is taking
     renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
     renderer.end_rendering()
+
+def get_target(self, packet: GameTickPacket) -> Vec3:
+    if self.maneuver == 'Attack':
+        return Vec3(packet.game_ball.physics.location)
+    else:
+        # Constant values can be found the the FieldInfo:
+        info = self.get_field_info()
+        
+        # Manually construct a list of all big boost pads
+        # info.boost_pads has a fixed size but info.num_boosts is how many pads there actually are
+        big_pads = []
+        for i in range(info.num_boosts):
+            pad = info.boost_pads[i]
+            if pad.is_full_boost:
+                big_pads.append(pad)
+
+        return Vec3(big_pads[0].location)
+
+def set_maneuver(self, packet: GameTickPacket):
+    if self.maneuver == None:
+        ball_location = Vec3(packet.game_ball.physics.location)
+
+        my_car = packet.game_cars[self.index]
+        car_location = Vec3(my_car.physics.location)
+
+        car_to_ball = ball_location - car_location
+
+        other_car = packet.game_cars[1]
+        opp_location = Vec3(other_car.physics.location)
+
+        opp_to_ball = ball_location - opp_location
+
+        if (abs(car_to_ball) < abs(opp_to_ball) | my_car.boost > 60):
+            self.maneuver = 'Attack'
+        else:
+            self.maneuver = 'GetBoostAtBack'
